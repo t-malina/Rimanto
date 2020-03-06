506d94d7e980f1b20aedcd413f9d2c6abaa89b0a
Kalkihe <t.malina@hacari.com>
2020-03-06 08:43:11 +0100

    implmented showing of create project panel

--- src/de/kalkihe/rimanto/presenter/EventProcessor.java
+++ src/de/kalkihe/rimanto/presenter/EventProcessor.java
@@ -1,8 +1,16 @@
 package de.kalkihe.rimanto.presenter;
 
+import de.kalkihe.rimanto.view.IRimantoView;
+
 public class EventProcessor implements IEventProcessor{
+  private IRimantoView rimantoView;
+
+  public EventProcessor(IRimantoView rimantoView) {
+    this.rimantoView = rimantoView;
+  }
+
   @Override
   public void newProjectButtonClick() {
-
+    this.rimantoView.startCreationOfProject();
   }
 }
