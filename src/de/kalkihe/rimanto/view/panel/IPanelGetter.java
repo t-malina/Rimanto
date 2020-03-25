a0c7d169ad9715ea14ba1ba9216b92b230e44b36
Kalkihe <t.malina@hacari.com>
2020-03-25 17:52:18 +0100

    comments view.panel

--- src/de/kalkihe/rimanto/view/panel/IPanelGetter.java
+++ src/de/kalkihe/rimanto/view/panel/IPanelGetter.java
@@ -5,17 +5,61 @@ import de.kalkihe.rimanto.model.data.IRisk;
 
 import javax.swing.*;
 
+/**
+ * Interface for getting Panels
+ */
 public interface IPanelGetter {
-  /*
-   * Gets the Panel for the start view of the application
+  /**
+   *
+   * @return The Panel for start of application
+   * @throws Exception
    */
   JPanel getPanelForOverview() throws Exception;
-  JPanel getPanelForProjectView(IProject project) throws Exception;
-  JPanel getPanelForRiskView(IProject project, IRisk risk) throws Exception;
-  JPanel getPanelForRiskCreation(IProject project) throws Exception;
-  JPanel getPanelForProjectInput();
-  JPanel getPanelForProjectEditing(IProject project);
-  JPanel getPanelForErrorView();
 
+  /**
+   *
+   * @param project The project to view
+   * @return The project view panel for passed project
+   * @throws Exception
+   */
+  JPanel getPanelForProjectView(IProject project) throws Exception;
+
+  /**
+   *
+   * @param project The project the risk belongs to
+   * @param risk The risk to view
+   * @return The risk view panel for the passed risk
+   * @throws Exception
+   */
+  JPanel getPanelForRiskView(IProject project, IRisk risk) throws Exception;
+
+  /**
+   *
+   * @param project The project the risk is to create for
+   * @return The panel for the cration of a risk
+   * @throws Exception
+   */
+  JPanel getPanelForRiskCreation(IProject project) throws Exception;
+
+  /**
+   *
+   * @return The panel for creating a project
+   */
+  JPanel getPanelForProjectInput();
+
+  /**
+   *
+   * @param project The project to edit
+   * @return The panel to edit the passed project
+   */
+  JPanel getPanelForProjectEditing(IProject project);
+
+  /**
+   *
+   * @param project The project which risk is to be exported
+   * @param risk The risk to export
+   * @return The panel fo export the passed risk as work instruction
+   * @throws Exception
+   */
   JPanel getPanelForExportRisk(IProject project, IRisk risk) throws Exception;
 }
--- src/de/kalkihe/rimanto/view/panel/PanelGetter.java
+++ src/de/kalkihe/rimanto/view/panel/PanelGetter.java
@@ -9,11 +9,20 @@ import de.kalkihe.rimanto.view.panel.panels.*;
 
 import javax.swing.*;
 
+/**
+ * Implementation of panel getter
+ */
 public class PanelGetter implements IPanelGetter{
   private IWordbook wordbook;
   private IEventProcessor eventProcessor;
   private IRimantoView rimantoView;
 
+  /**
+   * Constructor. Initializes needed references
+   * @param wordbook Wordbook for labels etc
+   * @param eventProcessor Eventhandler to pass events to
+   * @param rimantoView reference to view
+   */
   public PanelGetter(IWordbook wordbook, IEventProcessor eventProcessor, IRimantoView rimantoView)
   {
     this.wordbook = wordbook;
@@ -21,44 +30,74 @@ public class PanelGetter implements IPanelGetter{
     this.rimantoView = rimantoView;
   }
 
-  /*
-   * Creates new Overview Panel and returns it
+  /**
+   *
+   * @return The Panel for start of application
+   * @throws Exception
    */
   @Override
   public JPanel getPanelForOverview() throws Exception {
     return new OverviewPanel(this.wordbook, this.eventProcessor, this.rimantoView);
   }
 
+  /**
+   *
+   * @param project The project to view
+   * @return The project view panel for passed project
+   * @throws Exception
+   */
   @Override
   public JPanel getPanelForProjectView(IProject project) throws Exception {
     return new ProjectViewPanel(this.wordbook, this.eventProcessor, this.rimantoView, project);
   }
 
+  /**
+   *
+   * @param project The project the risk belongs to
+   * @param risk The risk to view
+   * @return The risk view panel for the passed risk
+   * @throws Exception
+   */
   @Override
   public JPanel getPanelForRiskView(IProject project, IRisk risk) throws Exception {
     return new RiskViewPanel(this.wordbook, this.eventProcessor, this.rimantoView, project, risk);
   }
 
+  /**
+   *
+   * @return The panel for creating a project
+   */
   @Override
   public JPanel getPanelForRiskCreation(IProject project) throws Exception {
     return new RiskViewPanel(this.wordbook, this.eventProcessor, this.rimantoView, project);
   }
 
+  /**
+   *
+   * @return The panel for creating a project
+   */
   @Override
   public JPanel getPanelForProjectInput() {
     return new CreateProjectPanel(this.wordbook, this.eventProcessor, this.rimantoView);
   }
 
+  /**
+   *
+   * @param project The project to edit
+   * @return The panel to edit the passed project
+   */
   @Override
   public JPanel getPanelForProjectEditing(IProject project) {
     return new CreateProjectPanel(this.wordbook, this.eventProcessor, this.rimantoView, project);
   }
 
-  @Override
-  public JPanel getPanelForErrorView() {
-    return null;
-  }
-
+  /**
+   *
+   * @param project The project which risk is to be exported
+   * @param risk The risk to export
+   * @return The panel fo export the passed risk as work instruction
+   * @throws Exception
+   */
   @Override
   public JPanel getPanelForExportRisk(IProject project, IRisk risk) throws Exception {
     return new InstructionGeneratorPanel(this.wordbook, this.eventProcessor, this.rimantoView, project, risk);
