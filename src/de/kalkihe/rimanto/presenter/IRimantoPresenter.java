f273a898b92a8b39b8505e0a993f6865db48cd05
Thomas Malina <github@malina.ws>
2026-04-20 15:42:39 +0200

    implemented first jpanel

---
+++ src/de/kalkihe/rimanto/presenter/IRimantoPresenter.java
@@ -0,0 +1,4 @@
+package de.kalkihe.rimanto.presenter;
+
+public interface IRimantoPresenter {
+}
---
+++ src/de/kalkihe/rimanto/view/frames/RimantoMainFrameOLD.java
@@ -0,0 +1,77 @@
+package de.kalkihe.rimanto.view.frames;
+
+import de.kalkihe.rimanto.view.wordbook.IWordbook;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RimantoMainFrame extends JFrame {
+  /*
+   * Other needed classes
+   */
+  private IWordbook wordbook;
+
+  /*
+   * UI-elements
+   */
+  private JPanel mainPanel;
+  private JButton button2;
+  private JButton button5;
+  private JTable table1;
+  private JButton testButton;
+  private JScrollPane jScrollPane;
+  private JMenuBar menuBar;
+
+  public RimantoMainFrame(IWordbook wordbook) {
+    this.wordbook = wordbook;
+    init();
+  }
+
+  private void init() {
+    // Set Title of Main window
+    this.setTitle("Rimanto");
+    // Set icon of the main window
+    ImageIcon icon = new ImageIcon("/windows/Nextcloud/Studium/Module/T3_3101 Studienarbeit/Rimanto/images/danger.png");
+    this.setIconImage(icon.getImage());
+    // Set initial size of the current window
+    this.setSize(600, 500);
+    // Set default close operation
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // Initialize menu bar
+    this.initializeMenuBar();
+
+    /*String data[][]={ {"101","Amit","670000"},
+      {"102","Jai","780000"},
+      {"101","Sachin","700000"}};
+    String column[]={"ID","NAME","SALARY"};
+
+    table1 = new JTable(data, column);
+    jScrollPane = new JScrollPane(table1);
+    mainPanel.add(jScrollPane, BorderLayout.CENTER);
+    /*
+
+    mainPanel.add(table1);
+*/
+    this.setJPanel(this.mainPanel);
+  }
+
+  private void initializeMenuBar() {
+    menuBar = new JMenuBar();
+    JMenu fileMenu = new JMenu(wordbook.getWordForWithCapitalLeadingLetter("file"));
+    JMenu helpMenu = new JMenu(wordbook.getWordForWithCapitalLeadingLetter("help"));
+    menuBar.add(fileMenu);
+    menuBar.add(helpMenu);
+    this.setJMenuBar(menuBar);
+  }
+
+  public JPanel getPanel()
+  {
+    return mainPanel;
+  }
+
+  public void setJPanel(JPanel panel)
+  {
+    this.add(panel);
+  }
+}
+
