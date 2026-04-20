dadaa1f1816a40455136ef8a1bab6a5f06750ecd
Thomas Malina <github@malina.ws>
2026-04-20 15:42:40 +0200

    comments view.panel.keyevent

--- src/de/kalkihe/rimanto/view/panel/keyevent/TabKeyAdapter.java
+++ src/de/kalkihe/rimanto/view/panel/keyevent/TabKeyAdapter.java
@@ -4,18 +4,31 @@ import javax.swing.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+/**
+ * Extension of KeyAdapter for tabbing through input elements
+ */
 public class TabKeyAdapter extends KeyAdapter {
   private JComponent nextComponent;
 
+  /**
+   * Connstructor. Initalizes the next component
+   * @param nextComponent
+   */
   public TabKeyAdapter(JComponent nextComponent) {
     super();
     this.nextComponent = nextComponent;
   }
 
+  /**
+   * Event if key was pressed
+   * @param e The key event
+   */
   @Override
   public void keyPressed(KeyEvent e) {
+    // If tab was pressed
     if(e.getKeyChar() == KeyEvent.VK_TAB)
     {
+      // Grab the focus of the next component that was passed
       nextComponent.grabFocus();
     }
   }
