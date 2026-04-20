e1bf197da50f52d43c0b99d8e931e076687a002f
Thomas Malina <github@malina.ws>
2026-04-20 15:42:40 +0200

    comments presenter

--- src/de/kalkihe/rimanto/presenter/IRimantoPresenter.java
+++ src/de/kalkihe/rimanto/presenter/IRimantoPresenter.java
@@ -6,14 +6,24 @@ import de.kalkihe.rimanto.model.data.IRisk;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Interface for the presenter
+ */
 public interface IRimantoPresenter {
   /*
    * Method to request list of all projects in model
    */
+
+  /**
+   * Method to request list of all projects in model
+   * @return List of all current projects
+   */
   List<IProject> fetchProjects();
 
-  /*
+  /**
    * Method to request list of all risks for a given project
+   * @param project Project to fetch risks for
+   * @return List of all risks of the passed project
    */
   List<IRisk> fetchRisksOfProject(IProject project);
 
